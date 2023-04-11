d4395ce8e3fe2fb91bb6a2a7635327ad199b0bd5
ichito <ichito@192.168.111.187>
2023-04-11 12:45:33 +0900

    task2_一次コミット

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.co.seattle.library.dto.UserInfo;
 import jp.co.seattle.library.service.UsersService;
+
+
 
 /**
  * ログインコントローラー
@@ -42,8 +45,12 @@ public class LoginController {
 		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
+		if(!(StringUtils. isEmpty(selectedUserInfo))){
+			return "redirect:/home";
+		} else {
+			model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
+			return "login";
+		}
 
-		
-		return "redirect:/home";
 	}
 }
